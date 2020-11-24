2477995d3dc246691fe60852354f8168618a38c6
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2020-11-24 14:06:55 +0500

    Initial Commit

---
+++ build/precache-manifest.13e81cf2eee258dcb35c52115b7dbdf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea68fd9bb56cdc7bb0b013b5978be44c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fefe2326b7fb95da1c4",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "5d5a9c5d61c1245cc84b",
+    "url": "/static/css/main.314ccece.chunk.css"
+  },
+  {
+    "revision": "5fefe2326b7fb95da1c4",
+    "url": "/static/js/2.f89fd00f.chunk.js"
+  },
+  {
+    "revision": "8819d044fc9cdbcc358fde839f0afde6",
+    "url": "/static/js/2.f89fd00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5a9c5d61c1245cc84b",
+    "url": "/static/js/main.0a2a9fb5.chunk.js"
+  },
+  {
+    "revision": "db8ad4e413a0741dc519",
+    "url": "/static/js/runtime-main.5a95b0c9.js"
+  }
+]);
